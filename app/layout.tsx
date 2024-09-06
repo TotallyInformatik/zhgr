dcbcf19c447cac311627f9fc4402c4e297539f7e
TotallyInformatik <totallyturing@gmail.com>
2024-09-06 19:22:06 +0200

    header forced to work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Head from "next/head";
-import Link from "next/link";
 import { Footer, TLink } from "@/components";
 
 
 export const metadata: Metadata = {
-  title: "zhangrui",
+  title: "zhang | rui",
   description: "Rui Zhang - Teaching Assistant ETHZ",
 };
 
