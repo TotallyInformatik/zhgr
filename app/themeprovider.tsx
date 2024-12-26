be48927e005bb91c314075db5a7b3400d710e553
TotallyInformatik <totallyturing@gmail.com>
2024-12-26 21:55:41 +0100

    no dont do the theme switcher yet

--- app/themeprovider.tsx
+++ app/themeprovider.tsx
@@ -5,6 +5,7 @@ import { ReactNode } from "react";
 export default function ThemeProvider({children} : {
   children: ReactNode,
 }) {
+  return <>{children}</>;
   return <NextThemesProvider 
     attribute="class"
     defaultTheme="system"
